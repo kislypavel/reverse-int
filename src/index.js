4f1bb6f84c86c7fd75cd4b18da42f2240d24b9e3
Pavel Kisly <kislypavel@gmail.com>
2023-01-27 19:20:44 +0300

    feat: implemitation alghoritm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    const num = String(n).replace('-', '').split('')
+    const count = num.length
+
+    let rev = ''
+
+    for (let i = 0; i < count; i++ ){
+        rev += num[(count - i) -1]
+    }
+
+    return rev
 }
